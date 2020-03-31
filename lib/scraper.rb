99e8574f304e28b056819fc4eca785285f96eae4
Monica Gerard <mlgerardvla@gmail.com>
2020-03-31 13:59:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,21 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
+
+    doc.css("div.roster-cards-container")
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
