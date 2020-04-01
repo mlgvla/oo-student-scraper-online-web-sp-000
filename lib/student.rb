ad124b01f2ce208ee8aaf32d618fc9d5dfa074c9
Monica Gerard <mlgerardvla@gmail.com>
2020-04-01 20:52:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,8 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.all.each
+    hash = attributes_hash.each { |k, v| send.("#{k}=", v) }
+    hash
   end
 
   def self.all
